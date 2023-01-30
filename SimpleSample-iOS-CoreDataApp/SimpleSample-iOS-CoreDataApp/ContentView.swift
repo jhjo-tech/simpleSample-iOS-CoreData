5440225505e7e900ff93f80ebe9f59fed42687a0
jhjo-tech <jhjo.tech@gmail.com>
2023-01-30 20:24:47 +0900

    Add ContentView

--- SimpleSample-iOS-CoreDataApp/SimpleSample-iOS-CoreDataApp/ContentView.swift
+++ SimpleSample-iOS-CoreDataApp/SimpleSample-iOS-CoreDataApp/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Spacer()
+            
+            Image(systemName: "")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(.gray)
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Save Image")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+            }
+            .padding(20)
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+            
+            Button {
+                
+            } label: {
+                Text("Show Image")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+            }
+            .padding(20)
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+            
+            Spacer()
+            
+            Image(systemName: "")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(.gray)
+            
+            Spacer()
+            
         }
         .padding()
+        
     }
 }
 
